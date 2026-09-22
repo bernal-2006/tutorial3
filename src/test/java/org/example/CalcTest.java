dd5bf0d61a50c8980fd4603f0fdc920406ec8f5c
bernal-2006 <3696195614@qq.com>
2026-09-22 17:21:18 +0800

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -3,16 +3,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcTest {
+
     @Test
-    void testAdd() {
+    public void testAdd() {
         Calc calc = new Calc();
         assertEquals(3, calc.add(1,2));
     }
 
-    // 这个是故意失败的测试
     @Test
-    void testSubFail() {
+    public void testSubFail() {
         Calc calc = new Calc();
+        // 10-7=3，但断言等于5 → 这个测试一定会失败
         assertEquals(5, calc.sub(10,7));
     }
 }

c9fe4040962ad3023d6006574f5711fb64114c1d
bernal-2006 <3696195614@qq.com>
2026-09-22 17:11:01 +0800

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
 
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    public void testAdd() {
+        Calc calc = new Calc();
+        assertEquals(5, calc.add(2,3));
     }
 
+    // 新增这个减法测试！
+    @Test
+    public void testSubtraction() {
+        Calc calc = new Calc();
+        assertEquals(2, calc.subtract(4,2));
+    }
 }

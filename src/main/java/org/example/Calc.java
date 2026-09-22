270f810849449f04d058ba7e61f6303d3b9eeacc
bernal-2006 <3696195614@qq.com>
2026-09-22 17:25:51 +0800

    Update Calc.java

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Calc {
-    public int add(int a, int b){
-        return a + b;
+    public int add(int a,int b){
+        return a+b;
     }
-    public int sub(int a, int b){
-        return a - b;
+    public int sub(int a,int b){
+        return a-b;
     }
 }
